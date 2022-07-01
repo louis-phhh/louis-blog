6769c5a0fadff48f4128c574558a5361bbac8f27
Louis Phan <louis.phhh@gmail.com>
2022-07-02 01:05:09 +0700

    init

---
+++ pages/index.js
@@ -0,0 +1,49 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout, { siteTitle } from '../components/layout'
+import Date from '../components/date'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../utils/posts'
+
+export default function Home({ allPostsData }) {
+  const facebookLink = 'https://facebook.com/duchauu'
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hi guys, I'm Hau, I'm a software engineer. This site is for sharing things that might be useful. You can contact me on <a target={'_blank'} href={facebookLink}>Facebook</a>.
+        </p>
+        
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData,
+    },
+  }
+}
